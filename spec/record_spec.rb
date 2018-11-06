d4ae6db61942736c4da90c9924fa162bd1c7d397
Emma Albury <emma_albury@hotmail.com>
2018-11-06 11:59:37 +0000

    balance_after_deposit test working w mocking

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -2,11 +2,22 @@ require 'record'
 
 describe Record do
 
-  let(:record) { Record.new(Transaction.new) }
+  let(:transaction) { double :transaction}
+  let(:record) { Record.new(transaction) }
 
   describe '#balance_after_deposit' do
     it 'returns the current balance of your funds' do
-    expect(record.balance_after_deposit).to eq(500)
+      allow(transaction).to receive(:deposit).and_return(500)
+      expect(record.balance_after_deposit).to eq(500)
     end
   end
+
+  # describe '#balance_after_withdrawal' do
+  #   it 'returns the current balance of your funds' do
+  #     allow(transaction).to receive(:deposit).and_return(500)
+  #     allow(transaction).to receive(:withdraw).and_return(250)
+  #     expect(record.balance_after_withdrawal).to eq(250)
+  #   end
+  # end
+
 end
