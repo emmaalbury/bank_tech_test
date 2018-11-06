8593b2b45d939f524970e8a49d0fd64c55a6aa17
Emma Albury <emma_albury@hotmail.com>
2018-11-06 22:10:16 +0000

    created print_statement method

--- lib/record.rb
+++ lib/record.rb
@@ -1,20 +1,51 @@
+require_relative 'transaction'
+
 class Record
   attr_reader :balance
 
-  def initialize(transaction)
-    @transaction = transaction
+  def initialize
+    @transaction = Transaction.new
     @balance = 0
+    @statement = []
   end
 
   def balance_after_deposit
-    @balance += @transaction.deposit
+    deposit = @transaction.deposit
+    @balance += deposit
+    update_statement('deposit', deposit)
   end
 
   def balance_after_withdrawal
-    @balance -= @transaction.withdraw
+    withdraw = @transaction.withdraw
+    @balance -= withdraw
+    update_statement('withdraw', withdraw)
+  end
+
+  def update_statement(transaction_type, transaction_amount)
+    if transaction_type == 'withdraw'
+      @statement << { :date => date, :credit => "   ", :debit => transaction_amount,
+      :balance => @balance }
+    else
+      @statement << { :date => date, :credit => transaction_amount, :debit => "   ",
+      :balance => @balance }
+    end
+    return @statement[-1]
+  end
+
+  def date
+    t = Time.now
+    date = t.strftime('%d/%m/%y')
+    return date
+  end
+
+  def print_statement
+    puts 'date     || credit || debit  || balance'
+    @statement.each do |transaction|
+      printed_transaction = transaction[:date] + ' ||  '
+      printed_transaction += transaction[:credit].to_s + '   ||  '
+      printed_transaction += transaction[:debit].to_s + '   ||   '
+      printed_transaction += transaction[:balance].to_s
+      puts printed_transaction
+    end
   end
 end
-# date || credit || debit || balance
-# 14/01/2012 || || 500.00 || 2500.00
-# 13/01/2012 || 2000.00 || || 3000.00
-# 10/01/2012 || 1000.00 || || 1000.00
