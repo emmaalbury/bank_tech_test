e8a4432de33d204008d0815b7ceb382e6f6c03e0
Emma Albury <emma_albury@hotmail.com>
2018-11-06 16:38:59 +0000

    statement functionality added to deposit

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,19 +1,26 @@
 class Transaction
+  def initialize
+    @statement = []
+  end
 
   def deposit
-    puts "What amount would you like to deposit?"
+    puts 'What amount would you like to deposit?'
     deposit = gets.chomp.to_i
+    @statement << { :date => date, :credit => deposit }
   end
 
   def withdraw
-    puts "What amount would you like to withdraw?"
+    puts 'What amount would you like to withdraw?'
     withdraw = gets.chomp.to_i
   end
 
   def date
     t = Time.now
-    date = t.strftime("%d/%m/%y")
+    date = t.strftime('%d/%m/%y')
     return date
   end
-
+  # date || credit || debit || balance
+  # 14/01/2012 || || 500.00 || 2500.00
+  # 13/01/2012 || 2000.00 || || 3000.00
+  # 10/01/2012 || 1000.00 || || 1000.00
 end
