e63b663dd03bce6a34a91be69c9a7eaa013ad2b4
Emma Albury <emma_albury@hotmail.com>
2018-11-06 22:11:52 +0000

    refactored - moved methods and statement variable to record

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,27 +1,12 @@
 class Transaction
-  def initialize
-    @statement = []
-  end
 
   def deposit
     puts 'What amount would you like to deposit?'
     deposit = gets.chomp.to_i
-    @statement << { :date => date, :credit => deposit }
   end
 
   def withdraw
     puts 'What amount would you like to withdraw?'
     withdraw = gets.chomp.to_i
-    @statement << { :date => date, :debit => withdraw }
   end
-
-  def date
-    t = Time.now
-    date = t.strftime('%d/%m/%y')
-    return date
-  end
-  # date || credit || debit || balance
-  # 14/01/2012 || || 500.00 || 2500.00
-  # 13/01/2012 || 2000.00 || || 3000.00
-  # 10/01/2012 || 1000.00 || || 1000.00
 end
