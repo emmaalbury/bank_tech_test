38dc21a20c53d1b20cef106f614808a0982add46
Emma Albury <emma_albury@hotmail.com>
2018-11-08 11:14:32 +0000

    print_statement method tested

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -3,34 +3,36 @@ require 'record'
 describe Record do
 
   let(:transaction) { double :transaction }
-  let(:record) { Record.new }
+  subject(:record) { described_class.new }
+  describe '#balance_after_deposit' do
+    it 'returns the current balance of your funds' do
+      allow($stdin).to receive(:gets).and_return( '500' )
+      allow(record).to receive(:date).and_return('11/02/81')
+      expect(record.balance_after_deposit).to eq ({:date=>"11/02/81",
+      :credit=>500, :debit=>nil, :balance=>500,})
+    end
+  end
 
-  # describe '#balance_after_deposit' do
-  #   it 'returns the current balance of your funds' do
-  #     allow(record).to receive(:date).and_return('11/02/81')
-  #     allow(transaction).to receive(:deposit).and_return(500)
-  #     expect(record.balance_after_deposit).to eq ({:date=>"11/02/81",
-  #     :credit=>500, :debit=>nil, :balance=>500,})
-  #   end
-  # end
-  #
-  # describe '#balance_after_withdrawal' do
-  #   it 'returns the current balance of your funds' do
-  #     allow(record).to receive(:date).and_return('10/11/79')
-  #     allow(transaction).to receive(:withdraw).and_return(250)
-  #     expect(record.balance_after_withdrawal).to eq ({:date=>"10/11/79",
-  #     :credit=>nil, :debit=>250, :balance=>-250})
-  #   end
-  # end
+  describe '#balance_after_withdrawal' do
+    it 'returns the current balance of your funds' do
+      allow($stdin).to receive(:gets).and_return( '250' )
+      allow(record).to receive(:date).and_return('10/11/79')
+      expect(record.balance_after_withdrawal).to eq ({:date=>"10/11/79",
+      :credit=>nil, :debit=>250, :balance=>-250})
+    end
+  end
 
-#   describe '#print_statement' do
-#     it 'prints out statement' do
-#       allow(record.print_statement).to receive(:date).and_return('06/11/79')
-#       allow(record.print_statement).to receive(:credit).and_return(1000)
-#       allow(record.print_statement).to receive(:debit).and_return(" ")
-#       allow(record.print_statement).to receive(:balance).and_return(1000)
-#       expect(record.print_statement).to eq ('date     || credit || debit  || balance
-#       06/11/79 ||  1000   ||        ||  1000 ')
-#     end
-#   end
+  describe '#print_statement' do
+    it 'prints out statement' do
+      allow(record).to receive(:date).and_return('06/11/79')
+      allow($stdin).to receive(:gets).and_return( '1000' )
+      record.balance_after_deposit
+      expect { record.print_statement }.to output("date || credit || debit || balance\n"\
+      "06/11/79 || 1000 ||  || 1000\n").to_stdout
+    end
+  end
 end
+
+# allow(record).to receive_message_chain({}, {}, {}, {}).with(:date),
+# .with(:credit).with(:debit).with(:balance).and_return([{ :date => '06/11/79'},
+#   { :credit => '06/11/79'}, { :debit => '06/11/79'}, { :balance => '06/11/79'}])
